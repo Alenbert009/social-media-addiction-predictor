ec04247e50ec3196e9a491ea797056112c9bddac
Alenbert009 <152258509+Alenbert009@users.noreply.github.com>
2025-11-15 18:26:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,64 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+model = joblib.load("final_model.pkl")
+scaler = joblib.load("scaler.pkl")
 st.set_page_config(page_title="Social Media Addiction Predictor")
 st.title("📱 Social Media Addiction Predictor")
 st.write("Fill in your details to check your addiction level.")
-education = st.selectbox(
-    "Your Education Level:",
-    ["Undergraduate","Above Undergraduate","Below Undergraduate"]
-)
-area = st.selectbox(
-    "Your Area:",
-    ["Municipal Urban", "Metropolitan", "Rural"]
-)
-num_social_media = st.selectbox(
-    "Number of Social Media Apps You Use:",
-    [1, 2, 3, 4, 5]
-)
-hours_used = st.slider(
-    "Daily Hours Spent on Social Media:",
-    min_value=0,
-    max_value=7,
-    value=1
-)
+column_names = [
+    'How many hours per day you use social media?',
+    'When you are online, at how many social media you remain active:',
+    '1. Has anyone ever told you offensive dirty stories or jokes using social media? ',
+    '2. Has anyone ever tried to get you to talk about personal sexual things using social media? ',
+    '3. Has anyone ever said offensive things about how you look, your body, or your sex life using social media?',
+    "4. Has anyone using social media ever tried to have a romantic or sexual relationship even though you tried to let him know you didn't want to? ",
+    '5. Has anyone using social media ever kept asking you to meet or go on a date even after you said “no”?',
+    '6. Has anyone using social media ever made dirty remarks about women in general (for example saying that all women are whores)?',
+    '7. Has anyone ever called you a lesbian or a “dyke” on social media?',
+    '8. Has anyone using social media ever sent you  offensive “dirty” videos and/or images?',
+    '9. Has anyone ever left a sexual comment or posted a picture of you in using social media?',
+    '10. Has anyone ever tried to get you to watch him on a messenger or chat box?',
+    '11. Has anyone ever tried to get you to have virtual sex over any social media which have instant messaging facility?',
+    '12. Has anyone ever sent you spam that advertised for pornographic Websites using social media?',
+    '13. Has anyone ever sent you pop-ups with links to pornographic Web sites?',
+    '14. Has anyone ever   left an offensive “dirty” comment on the wall of social media?',
+    '15. Has anyone ever sent messages that refer to or ask about your body, sex life, or intimate subjects through social media (for example asking if you are on your period right now))?',
+    '16. Has anyone ever threaten to hack your computer/mobile and cause damage if you did not conduct the sexual act requested for (for example describing giving the person oral sex)?',
+    '17. Has anyone ever sent frightening message through social to get you to perform sexual acts (for example cyber-sex)?_le',
+    '18. Has anyone ever bribed you for conducting sexual acts (for example offering to send you money if you send him/her sexual obscene pictures)?',
+    '19. Using Social Media, has anyone ever sexually harassed you?',
+    'Facebook',
+    'Instagram',
+    'Others',
+    'Twitter',
+    'Whatsapp'
+]
+user_input = []
+st.write("### 🔹 For harassment-related questions:")
+st.write("Use: **0 = Never, 1 = Sometimes, 2 = Once or twice**")
+for col in column_names:
+    if col in ["Facebook", "Instagram", "Others", "Twitter", "Whatsapp"]:
+        value = st.selectbox(f"{col} (0 = No, 1 = Yes)", [0, 1])
+    elif col == "How many hours per day you use social media?":
+        value = st.slider(col, 0.0, 7.0, 1.0, step=0.5)
+    elif col == "When you are online, at how many social media you remain active:":
+        value = st.slider(col, 0, 5, 1)
+    else:
+        value=st.selectbox(col,[0, 1, 2])
+    user_input.append(value)
+final_input=np.array(user_input).reshape(1,-1)
+scaled_input=scaler.transform(final_input)
+
 if st.button("Predict Addiction Level"):
-     if hours_used >= 6 or (num_social_media>4 and hours_used in [3,4,5]):
+     prediction = model.predict(scaled_input)[0]
+     if prediction == 1:
+        st.success("⚠️ **Moderately Addicted** — Noticeable usage pattern.")
+     elif prediction == 2:
         st.error("🚨 **Highly Addicted** — Very high usage and multiple apps.")
-     elif 2 <= hours_used <= 6:
-        st.warning("⚠️ **Moderately Addicted** — Noticeable usage pattern.")
-     else:
-        st.success("✅ **Not Addicted** — Usage seems normal.")
-        
-     st.subheader("🔍 Your Input Data")
-     st.write(f"**Education:** {education}")
-     st.write(f"**Area:** {area}")
-     st.write(f"**Number of Social Media Apps:** {num_social_media}")
-     st.write(f"**Daily Hours Used:** {hours_used} hours")
+     elif prediction == 3:
+        st.info("✅ **Not Addicted** — Usage seems normal.")  
+
 
 
